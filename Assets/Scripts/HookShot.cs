0cf2e519449c318c784adc2f501501120bdf4e17
Brandon Williams <bwei.williams@gmail.com>
2016-06-22 01:12:04 -0600

    Working grappling hook
    continuous force along pull needs to be implemented still

--- Assets/Scripts/HookShot.cs
+++ Assets/Scripts/HookShot.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using WindowsInput;
 
 public class HookShot : MonoBehaviour {
 
@@ -8,16 +9,19 @@ public class HookShot : MonoBehaviour {
 	public float smoothing = 5f;
 
 	private bool anchored = false;
-	private float distance;
+	private bool firstPullFrame = true;
 	private Vector3 delta;
 	private Rigidbody hookRb;
 	private Rigidbody playerRb;
-
+	private UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController playerController;
 
 	void Start () 
 	{
 		hookRb = GetComponent <Rigidbody> ();
 		playerRb = GameObject.FindGameObjectWithTag ("Player").GetComponent <Rigidbody> ();
+		playerController = playerRb.GetComponent <UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController> ();
+
+		hookRb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
 
 		Invoke("Expire", lifetime);
 	}
@@ -25,32 +29,40 @@ public class HookShot : MonoBehaviour {
 	void OnCollisionEnter (Collision col) 
 	{
 		hookRb.velocity = Vector3.zero;
+		InputSimulator.SimulateKeyPress (VirtualKeyCode.SPACE);
 		anchored = true;
-
-		playerRb.AddForce(Vector3.up * 50, ForceMode.Impulse);
-		playerRb.AddForce ((transform.position - playerRb.position).normalized * strength, ForceMode.VelocityChange);
-//		playerRb.velocity = (transform.position - playerRb.position).normalized * strength;
+		playerRb.collisionDetectionMode = CollisionDetectionMode.Continuous;
 	}
 
 	void FixedUpdate ()
 	{
-		if (anchored) {
+		if (anchored && !playerController.Grounded) {
 			PullPlayer ();
 		}
 	}
 
 	void PullPlayer () 
 	{
-		distance = (playerRb.position - transform.position).magnitude;
-		if (distance > 2) {
-			playerRb.AddForce ((transform.position - playerRb.position).normalized * strength);
-		} else {
-			delta = playerRb.position - transform.position;
-			delta.y = 0;
-			playerRb.AddForce ((playerRb.position - transform.position).normalized * smoothing, ForceMode.VelocityChange);
-//			playerRb.velocity = Vector3.up + delta.normalized;
+		Vector3 deltaPos = transform.position - playerRb.position;
+		float distance = deltaPos.magnitude;
 
-			Destroy (gameObject);
+		if (firstPullFrame) {
+			playerRb.AddForce (deltaPos.normalized * strength, ForceMode.Impulse);
+			firstPullFrame = false;
+		}
+
+		if (distance < 1) {
+			playerRb.AddForce (Vector3.zero, ForceMode.VelocityChange);
+			anchored = false;
+			Expire ();
+		} else if (distance <= 5) {
+//			delta = playerRb.position - transform.position;
+//			delta.y = 0;
+//			playerRb.AddForce ((playerRb.position - transform.position).normalized * smoothing, ForceMode.VelocityChange);
+//			playerRb.velocity = Vector3.up + delta.normalized;
+			playerRb.velocity *= 0.9f;
+		} else {
+			playerRb.AddForce (deltaPos.normalized, ForceMode.VelocityChange);
 		}
 	}
 
