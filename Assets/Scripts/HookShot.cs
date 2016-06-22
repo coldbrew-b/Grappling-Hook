fcd38ab1578d37d709f296ad4ed33250fdf7ab07
Brandon Williams <bwei.williams@gmail.com>
2016-06-22 03:13:39 -0600

    consistent pull speeds
    everything we saw in the preview

--- Assets/Scripts/HookShot.cs
+++ Assets/Scripts/HookShot.cs
@@ -6,14 +6,12 @@ public class HookShot : MonoBehaviour {
 
 	public float lifetime = 1f;
 	public float strength = 100f;
-	public float smoothing = 5f;
 
 	private bool anchored = false;
-	private bool firstPullFrame = true;
 	private Vector3 delta;
 	private Rigidbody hookRb;
 	private Rigidbody playerRb;
-	private UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController playerController;
+	private UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController playerController;		
 
 	void Start () 
 	{
@@ -29,14 +27,17 @@ public class HookShot : MonoBehaviour {
 	void OnCollisionEnter (Collision col) 
 	{
 		hookRb.velocity = Vector3.zero;
-		InputSimulator.SimulateKeyPress (VirtualKeyCode.SPACE);
+		if (playerController.Grounded) {
+			InputSimulator.SimulateKeyPress (VirtualKeyCode.SPACE);
+		}
+
 		anchored = true;
 		playerRb.collisionDetectionMode = CollisionDetectionMode.Continuous;
 	}
 
 	void FixedUpdate ()
 	{
-		if (anchored && !playerController.Grounded) {
+		if (anchored) {
 			PullPlayer ();
 		}
 	}
@@ -46,23 +47,12 @@ public class HookShot : MonoBehaviour {
 		Vector3 deltaPos = transform.position - playerRb.position;
 		float distance = deltaPos.magnitude;
 
-		if (firstPullFrame) {
-			playerRb.AddForce (deltaPos.normalized * strength, ForceMode.Impulse);
-			firstPullFrame = false;
-		}
-
-		if (distance < 1) {
-			playerRb.AddForce (Vector3.zero, ForceMode.VelocityChange);
+		if (distance < 2) {
+			playerRb.velocity = Vector3.zero;
 			anchored = false;
 			Expire ();
-		} else if (distance <= 5) {
-//			delta = playerRb.position - transform.position;
-//			delta.y = 0;
-//			playerRb.AddForce ((playerRb.position - transform.position).normalized * smoothing, ForceMode.VelocityChange);
-//			playerRb.velocity = Vector3.up + delta.normalized;
-			playerRb.velocity *= 0.9f;
 		} else {
-			playerRb.AddForce (deltaPos.normalized, ForceMode.VelocityChange);
+			playerRb.velocity = deltaPos.normalized * strength;
 		}
 	}
 
